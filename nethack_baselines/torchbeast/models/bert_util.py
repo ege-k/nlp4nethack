8284e4a34ee8c4ab936f102e104a859f41fd778f
Johannes <jdolllinger@student.ethz.ch>
2021-08-06 19:27:51 +0200

    fixed size issues

--- nethack_baselines/torchbeast/models/bert_util.py
+++ nethack_baselines/torchbeast/models/bert_util.py
@@ -1,28 +1,57 @@
 import torch
 from NetHackCorpus.corpus_loader import load_corpus
+from tqdm import trange
+import psutil
 
-from transformers import BertModel, BertTokenizer
+from transformers import BertModel, BertTokenizer, logging
 
 
 def corpus_as_bert_embeddings(corpus_dir, device, output_device="cpu", cleaned=False):
+
+    print("Available space:", psutil.virtual_memory().total)
+
+    free = psutil.virtual_memory().available
     if cleaned:
         corpus = load_corpus(corpus_dir, cleaned=cleaned)
     else:
         manual, wiki, spoilers = load_corpus(corpus_dir, cleaned=cleaned)
         corpus = manual + wiki + spoilers
+        del manual
+        del wiki
+        del spoilers
+    corpus_size = free - psutil.virtual_memory().available
 
+    logging.set_verbosity_error()
+
+    free = psutil.virtual_memory().available
     model = BertModel.from_pretrained('bert-base-uncased').to(device)
     model.eval()
     tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+    bert_size = free - psutil.virtual_memory().available
+
+    print("Corpus takes:", corpus_size, "Bert takes", bert_size)
 
     filename_to_bert = dict()
-    for doc in corpus:
+    t = trange(len(corpus), desc='Bar desc', leave=True)
+    free = psutil.virtual_memory().available
+
+    a = torch.ones([1, 768], dtype=torch.double)
+    print(free-psutil.virtual_memory().available)
+    free = psutil.virtual_memory().available
+
+    for idx in t:
+        doc = corpus[idx]
         name = doc.split(" ")[0]
 
         input_ids = tokenizer.encode(doc)[:512]
-        input_ids = torch.tensor(input_ids).unsqueeze(0).to(device)
-        pooled_output = model(input_ids).pooler_output  # vec of size 1 x 768
+        input_ids = torch.tensor(input_ids, dtype=torch.long).unsqueeze(0).to(device)
+        with torch.no_grad():
+            pooled_output = model(input_ids).pooler_output  # vec of size 1 x 768
         filename_to_bert[name] = pooled_output.to(output_device)
+
+        free_2 = psutil.virtual_memory().available
+        t.set_description("Bar desc (file %i), average size: %i" % (idx, (free-free_2)/(idx+1)))
+        t.refresh()  # to show immediately the update
 
     return filename_to_bert
 
